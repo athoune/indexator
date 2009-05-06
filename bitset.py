34c81ce74989e5c7b46f6361b1a528eae5b588b5
Mathieu Lecarme <mathieu@garambrogne.net>
2009-05-06 15:49:58 +0200

    initial object

---
+++ bitset.py
@@ -0,0 +1,34 @@
+import marshal
+import zlib
+
+class BitSet:
+	_data = 0L
+	_size = 0
+	def __init__(self,array):
+		for element in array:
+			if element :
+				a = 1
+			else:
+				a = 0
+			self._data = (self._data << 1) | a
+			self._size += 1
+	def __str__(self):
+		return "<Bitset #%i %s>" % (self._size, str(bin(self._data))[2:])
+	def __and__(self, other):
+		pass
+	def __len__(self):
+		return self._size
+	def value(self):
+		return self._data
+
+if __name__ == '__main__':
+	import unittest
+	class SimpleTest(unittest.TestCase):
+		def setUp(self):
+			self.b = BitSet([True, True, False])
+		def testInit(self):
+			self.assert_(3, len(self.b))
+			self.assert_(6L, self.b.value())
+		def testDump(self):
+			self.assert_("<Bitset #3 110>" == str(self.b))
+	unittest.main()
