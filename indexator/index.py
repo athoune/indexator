56fc71f7ab56cfb999873b0a78f805c5905fde9c
Mathieu Lecarme <mathieu@garambrogne.net>
2009-05-19 19:45:24 +0200

    TokyoCabinet powa

--- indexator/index.py
+++ indexator/index.py
@@ -1,10 +1,45 @@
+# -*- coding: utf-8 -*-
 import collections
+import marshal
+try:
+	import pytc
+	TC = True
+except:
+	TC = False
 
 """
 [TODO] persistance
 [TODO] choix du moteur, gdbm, tokyo cabinet ...
 [TODO] création des index
 """
+
+class Data:
+	def __init__(self, file):
+		self.data = {}
+	def encodeKey(self, key):
+		return str(key)
+	def encodeValue(self, value):
+		return marshal.dumps(value)
+	def decodeValue(self, value):
+		return marshal.loads(value)
+	def __getitem__(self, item):
+		return self.decodeValue(self.data[self.encodeKey(item)])
+	def __setitem__(self, item, value):
+		self.data[self.encodeKey(item)] = self.encodeValue(value)
+	def __len__(self):
+		return len(self.data)
+	def __iter__(self):
+		for k,v in self.data:
+			yield k, self.decodeValue(v)
+
+if TC:
+	class TokyoCabinetData(Data):
+		def __init__(self, file):
+			self.data = pytc.HDB()
+			self.data.open(file, pytc.HDBOWRITER | pytc.HDBOCREAT)
+		def __repr__(self):
+			return '<TokyoCabinetData size:%i>' % (len(self))
+
 class Index(collections.Mapping):
 	_data = {}
 	_indexed = None
@@ -20,6 +55,17 @@ class Index(collections.Mapping):
 
 if __name__ == '__main__':
 	import unittest
+	
+	class DataTest(unittest.TestCase):
+		def testGetSet(self):
+			if TC:
+				d = TokyoCabinetData('/tmp/tc.htc')
+				data = [1,2,3,"a"]
+				d[42] = data
+				self.assert_(data, d[42])
+				self.assert_(1, d)
+				print d
+	
 	class IndexTest(unittest.TestCase):
 		def setUp(self):
 			pass
@@ -29,7 +75,7 @@ if __name__ == '__main__':
 			'tags' : ['sponge', 'sea'],
 			}
 			i = Index(m)
-			for k in i:
-				print k
+			#for k in i:
+			#	print k
 
 	unittest.main()
