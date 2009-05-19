1f3fdc2c39bd4d96675e60af8a69c378595363a8
Mathieu Lecarme <mathieu@garambrogne.net>
2009-05-19 17:25:09 +0200

    managing null size

--- indexator/log/apache.py
+++ indexator/log/apache.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 __doc__ = """
 """
 __author__ = "Mathieu Lecarme <mathieu@garambrogne.net>"
@@ -7,7 +8,7 @@ from datetime import datetime
 from browscap_tiny import UserAgent
 
 RE_COMMON = re.compile('(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (\d+)', re.U)
-RE_COMBINED = re.compile('(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (\d+) "(.*?)" "(.*?)"', re.U)
+RE_COMBINED = re.compile('(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (.*?) "([^"]*)" "([^"]*)"', re.U)
 userAgent = UserAgent()
 
 class Common:
@@ -24,10 +25,15 @@ class Common:
 		'size':   int(m.group(8))
 		}
 		return dico
-
+def intOrNull(a):
+	if a == '-': return None
+	return int(a)
 class Combined:
 	def parse(self, line):
 		m = RE_COMBINED.match(line)
+		if m == None:
+			print line
+			raise 
 		dico = {
 		'ip':     m.group(1),
 		'user':   m.group(2),
@@ -36,7 +42,7 @@ class Combined:
 		'url':    m.group(5),
 		'http':   m.group(6),
 		'code':   int(m.group(7)),
-		'size':   int(m.group(8)),
+		'size':   intOrNull(m.group(8)),
 		'referer':m.group(9),
 		'user-agent':userAgent.match(m.group(10))
 		}
@@ -46,18 +52,19 @@ if __name__ == '__main__':
 	import sys
 	if len(sys.argv) > 1:
 		c = Combined()
-		f = file(sys.argv[1], 'w')
+		f = file(sys.argv[1], 'r')
 		for line in f:
-			print c.parse(l)
-	import unittest
-	class ApacheTest(unittest.TestCase):
-		def testCommon(self):
-			log = '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
-			common = Common()
-			print common.parse(log)
-		def testCombined(self):
-			log = '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"'
-			common = Combined()
-			print common.parse(log)
-			
-	unittest.main()
+			print c.parse(line)
+	else:
+		import unittest
+		class ApacheTest(unittest.TestCase):
+			def testCommon(self):
+				log = '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
+				common = Common()
+				print common.parse(log)
+			def testCombined(self):
+				log = '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"'
+				common = Combined()
+				print common.parse(log)
+				
+		unittest.main()
